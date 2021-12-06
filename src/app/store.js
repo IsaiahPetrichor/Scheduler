e351d6ac1f9e2b8981e2be4009b7bc029438a14d
Isaiah Leach <30422715+FallenVFX@users.noreply.github.com>
2021-12-05 20:46:04 -0800

    Removed unused Redux Slice

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
 import employeeReducer from '../components/employees/employeeSlice';
-import scheduleReducer from '../components/scheduleEmployee/scheduleSlice';
 
 export default configureStore({
 	reducer: {
 		employees: employeeReducer,
-		schedule: scheduleReducer,
 	},
 });
